20269d81a05e86381afca23a005838b40cf3b2e0
shaielia <shaielia@gmail.com>
2021-12-16 20:13:40 +0200

    commit shaithkjfdsdfjbfbkat12jfng53

---
+++ my_boto.py
@@ -0,0 +1,33 @@
+client = boto3.client("ec2")
+
+
+def get_instance_ids(client):
+    my_instances = client.describe_instances()
+    instance_ids_to_return = []
+    for reservation in my_instances["Reservations"]:
+        for instance in reservation["Instances"]:
+            if instance["InstanceType"] == "t2.micro" and instance["State"]["Name"] == "running":
+                instance_ids_to_return.append({"id": instance["InstanceId"], "ip": instance["PublicIpAddress"]})
+    return instance_ids_to_return
+
+
+ec2 = boto3.resource('ec2')
+
+def create_instance(ec2):
+    response = ec2.create_instances(SubnetId="subnet-018523f5b12a8b3fb", InstanceType="t2.micro", ImageId="ami-0947d2ba12ee1ff75", MaxCount=1, MinCount=1)
+    return response
+
+def stop_my_instance(client, instance_to_stop):
+    response = client.stop_instances(InstanceIds=[instance_to_stop])
+    return response
+
+# create_instance(ec2)
+# print(get_instance_ids(client))
+# stop_my_instance(client, "i-0e498ecccade1943e")
+
+def set_eip_by_instance_id(client, instance_id):
+    allocation = client.allocate_address()
+    response = client.associate_address(AllocationId=allocation['AllocationId'], InstanceId=instance_id)
+    return response
+
+set_eip_by_instance_id(client, "i-0e498ecccade1943e")
